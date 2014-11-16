522d282bd1777868ec1dbaa25e78feb92d3c6008
Iran Reyes Fleitas <iranreyesfleitas@gmail.com>
2014-11-16 19:19:44 -0200

    Baby steps completed, ready for the third exercise.

--- program.js
+++ program.js
@@ -1,1 +1,8 @@
-console.log("HELLO WORLD");
+var counter = 0;
+
+for (var i = 2, len = process.argv.length; i < len; i++) {
+	//Optional => if(!isNaN(process.argv[i]))
+	counter += parseInt(process.argv[i]);
+};
+
+console.log(counter);
