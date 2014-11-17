994607b8ef144b9c00c45d10f3cdbae5bde2ce99
Iran Reyes Fleitas <iranreyesfleitas@gmail.com>
2014-11-16 23:11:36 -0200

    Last Exercise, JSON API Server

--- program.js
+++ program.js
@@ -1,7 +1,6 @@
 //Libraries
 var http = require('http');
-var map = require('through2-map');
-var fs = require('fs');
+var url = require('url');
 
 if(process.argv.length < 3)
 {
@@ -11,13 +10,34 @@ if(process.argv.length < 3)
 
 var server = http.createServer(function(request, response){
 
-	request.pipe(map(function(chunk){
-		return chunk.toString().toUpperCase();
-	})).pipe(response);
+	var myUrl = url.parse(request.url, true);
 
-	response.on('end', function(){
-		response.end();	
-	})
+	response.setHeader("Content-Type", "application/json");
+
+	if(myUrl.pathname == '/api/parsetime' && myUrl.query['iso'] != undefined){
+		var date = new Date(myUrl.query['iso']);
+		var json = {
+			"hour": date.getHours(),
+			"minute": date.getMinutes(),
+			"second":date.getSeconds()
+		}
+		response.statusCode = 200;
+		response.end(JSON.stringify(json));
+	}
+	else if(myUrl.pathname == '/api/unixtime')
+	{
+		var date = new Date(myUrl.query['iso']);
+		var json = {
+			"unixtime": date.getTime()
+		}
+		response.statusCode = 200;
+		response.end(JSON.stringify(json));
+	}
+	else
+	{
+		response.statusCode = 400;
+		response.end();
+	}
 });
 
 server.listen(process.argv[2]);
