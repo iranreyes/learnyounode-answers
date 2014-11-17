d7f337f32e31f4a8093dd4c72e652acbd2960b9b
Iran Reyes Fleitas <iranreyesfleitas@gmail.com>
2014-11-16 22:09:47 -0200

    Collecting data with http

--- program.js
+++ program.js
@@ -7,12 +7,19 @@ if(process.argv.length < 3)
 	return;
 }
 
+var data_return = "";
 http.get(process.argv[2], function(response){
 
 	response.setEncoding("utf8");
 
+	//Events
 	response.on("data", function(data){
-		console.log(data);
+		data_return+=data;
+	});
+
+	response.on("end", function(data){
+		console.log(data_return.length);
+		console.log(data_return);
 	});
 
 }).on('error', function(e) {
